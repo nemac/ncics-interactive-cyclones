f2b4ee875d44096fb52d46a54b230e0ed7d10b25
Matthew Geiger <mgeiger@unca.edu>
2021-11-15 16:37:36 -0500

    Query features in North Atlantic basin

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export const config = {
   yAxisTitle: 'Tropical Cyclone Days (per year)',
   stormTypes: {
     'named_storm': {
-      'where': 'USA_WIND>=34',
+      'where': "USA_WIND>=34 AND BASIN = 'NA'",
       'label': 'Named Storms',
       'tooltip': 'Named Storm Days',
       'legend_label': 'Named Storms (>= 34kt)',
@@ -17,7 +17,7 @@ export const config = {
       'active': true
     },
     'hurricane': {
-      'where': 'USA_WIND>=64',
+      'where': "USA_WIND>=64 AND BASIN = 'NA'",
       'label': 'Hurricanes',
       'tooltip': 'Hurricane Days',
       'legend_label': 'Hurricanes (>=64kt)',
@@ -25,7 +25,7 @@ export const config = {
       'active': true
     },
     'major_hurricane': {
-      'where': 'USA_WIND>=96',
+      'where': "USA_WIND>=96 AND BASIN = 'NA'",
       'label': 'Major Hurricanes',
       'tooltip': 'Major Hurricane Days',
       'legend_label': 'Major Hurricanes (>= 96kt)',
